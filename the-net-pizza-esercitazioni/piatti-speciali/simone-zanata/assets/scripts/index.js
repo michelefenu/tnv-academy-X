da520c2f965a928bcd6d3ed5c6ffacb2f5dcc8ba
SimoneZanata <simonezanata.88@gmail.com>
2023-07-05 00:59:54 +0200

    New features

--- the-net-pizza-esercitazioni/piatti-speciali/simone-zanata/assets/scripts/index.js
+++ the-net-pizza-esercitazioni/piatti-speciali/simone-zanata/assets/scripts/index.js
@@ -1,34 +1,35 @@
 
 
-function randomAarray(array) {
+    function randomArray(array) {
     return array.sort(() => Math.random() - 0.5);
 }
 
-const piattiRandom = [];
+    const newArray = [];
 
-
-const buildList = (piatti) => {
-    const testUlElement = document.getElementById('piatti-alternativi');
+    const buildList = (piatti) => {
+    const piattiAlternativi = document.getElementById('piatti-alternativi');
     const antipasti = piatti.filter(x => x.category === 'antipasti');
     const primi = piatti.filter(x => x.category === 'primi');
-    piattiRandom.push(...antipasti);
-    piattiRandom.push(...primi);
-    randomAarray(piattiRandom);
+    newArray.push(...antipasti);
+    newArray.push(...primi);
+    randomArray(newArray);
+    const shuffledArray=newArray.slice(0,3);
 
-    for (let i = 0; i <= 2; i++) {
+
+    for (let piatto of shuffledArray) {
 
         const card = document.createElement('div');
         card.classList.add('col-6', 'col-sm-6', 'col-md-4', 'py-2');
         card.innerHTML = `
          <div class="card" style="width: 13rem;">
-         <img src="${piattiRandom[i].imageUrl}" class="card-img-top" alt="${piattiRandom[i].title}">
+         <img src="${piatto.imageUrl}" class="card-img-top" alt="${piatto.title}">
          <div class="card-body">
          <p class="card-text">
-              ${piattiRandom[i].title}
+              ${piatto.title}
           </p>
          </div>
          </div>`;
-        testUlElement.appendChild(card);
+        piattiAlternativi.appendChild(card);
     }
 
 }
