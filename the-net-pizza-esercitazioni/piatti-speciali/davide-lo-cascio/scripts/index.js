1f791b7bfd17d187011dc63a90572d8d75f56249
Davide <davidelcd.90@gmail.com>
2023-07-04 19:26:53 +0200

    hi2

--- the-net-pizza-esercitazioni/piatti-speciali/davide-lo-cascio/scripts/index.js
+++ the-net-pizza-esercitazioni/piatti-speciali/davide-lo-cascio/scripts/index.js
@@ -1,41 +1,20 @@
 const buildMenu = (piatti) => {
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-    
-    piatti = shuffleArray(piatti);
-    piatti = piatti.slice(0, 3);
+    const sectionNames = ['antipasti', 'primi', 'dolci'];
 
-    const categories = [...new Set(piatti.map(x => x.category))];
+    sectionNames.forEach((sectionName, index) => {
+        const section = document.getElementById(sectionName);
+        const startIndex = index * 3;
+        const endIndex = startIndex + 3;
+        const cards = piatti.slice(startIndex, endIndex);
 
-    const menuSections = document.getElementById('menuSections');
-
-    for (let category of categories) {
-        const sectionTitle = document.createElement('div');
-        sectionTitle.classList.add('col-12', 'pb-2');
-        sectionTitle.innerHTML = `<h3>${category}</h3>`;
-        menuSections.appendChild(sectionTitle);
-
-        const sectionPiattiDiv = document.createElement('div');
-        sectionPiattiDiv.classList.add('row');
-        sectionPiattiDiv.id = category;
-        menuSections.appendChild(sectionPiattiDiv);
-
-        const sectionPiatti = piatti.filter(x => x.category === category);
-        buildSection(sectionPiatti, `${category}`);
-    }
+        buildSection(cards, sectionName, section);
+    });
 
     document.getElementById('menu').style.display = 'block';
     document.getElementById('loadingMessage').style.display = 'none';
 }
 
-const buildSection = (piatti, sectionName) => {
-    const section = document.getElementById(sectionName);
-
+const buildSection = (piatti, sectionName, section) => {
     for (let piatto of piatti) {
         const card = document.createElement('div');
         card.classList.add('col-12', 'col-sm-6', 'col-md-4', 'py-2');
@@ -63,7 +42,7 @@ const buildSection = (piatti, sectionName) => {
     }
 }
 
-fetch('http://my-json-server.typicode.com/michelefenu/tnv-academy-X/piatti')
+fetch('https://my-json-server.typicode.com/michelefenu/tnv-academy-X/piatti')
     .then((response) => response.json())
     .then((response) => buildMenu(response))
     .catch((error) => console.log('ERRORE', error));
--- the-net-pizza-javascript/assets/scripts/index.js
+++ the-net-pizza-javascript/assets/scripts/index.js
@@ -1,58 +1,52 @@
-/* const buildList = (piatti) => {
-    const testUlElement = document.getElementById('test');
-
-    for (let piatto of piatti) {
-        const liElement = document.createElement('li');
-        liElement.innerText = piatto.title;
-
-        testUlElement.appendChild(liElement);
-    }
-} */
-
 const buildMenu = (piatti) => {
     const antipasti = piatti.filter(x => x.category === 'antipasti');
     const primi = piatti.filter(x => x.category === 'primi');
     const dolci = piatti.filter(x => x.category === 'dolci');
-
+  
     buildSection(antipasti, 'antipasti');
     buildSection(primi, 'primi');
     buildSection(dolci, 'dolci');
-
+  
     document.getElementById('menu').style.display = 'block';
     document.getElementById('loadingMessage').style.display = 'none';
-}
-
-const buildSection = (piatti, sectionName) => {
+  };
+  
+  const buildSection = (piatti, sectionName) => {
     const section = document.getElementById(sectionName);
-    
-    for(let piatto of piatti) {
-        const card = document.createElement('div');
-        card.classList.add('col-12', 'col-sm-6', 'col-md-4', 'py-2');
-        
-        if(!piatto.available) {
-            card.style.opacity = '0.5';
-        }
-
-        card.innerHTML = `
-            <div class="card shadow">
-                <img
-                    src="${piatto.imageUrl}"
-                    class="card-img-top"
-                    alt="${piatto.title}"
-                />
-                <div class="card-body">
-                    <h5 class="card-title">${piatto.title} - ${piatto.price}€</h5>
-                    <p class="card-text">
-                      ${piatto.description}
-                    </p>
-                </div>
-            </div>`;
-
-        section.appendChild(card);
+  
+    for (let piatto of piatti) {
+      const card = document.createElement('div');
+      card.classList.add('col-12', 'col-sm-6', 'col-md-4', 'py-2');
+  
+      if (!piatto.available) {
+        card.style.opacity = '0.5';
+      }
+  
+      const img = document.createElement('img');
+      img.src = piatto.imageUrl;
+      img.classList.add('card-img-top');
+      img.alt = piatto.title;
+  
+      const cardBody = document.createElement('div');
+      cardBody.classList.add('card-body');
+  
+      const title = document.createElement('h5');
+      title.classList.add('card-title');
+      title.innerText = `${piatto.title} - ${piatto.price}€`;
+  
+      const description = document.createElement('p');
+      description.classList.add('card-text');
+      description.innerText = piatto.description;
+  
+      cardBody.appendChild(title);
+      cardBody.appendChild(description);
+      card.appendChild(img);
+      card.appendChild(cardBody);
+      section.appendChild(card);
     }
-}
-
-fetch('http://my-json-server.typicode.com/michelefenu/tnv-academy-X/piatti')
+  };
+  
+  fetch('http://my-json-server.typicode.com/michelefenu/tnv-academy-X/piatti')
     .then((response) => response.json())
     .then((response) => buildMenu(response))
     .catch((error) => console.log('ERRORE', error));
